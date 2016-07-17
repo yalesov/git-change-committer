541c844571f3effdb997c5b10da333f7a4296e10
Yulij Andreevich Lesov <yalesov@gmail.com>
2016-07-18 00:30:37 +0800

    fix script to correctly use variables

--- change.sh
+++ change.sh
@@ -11,15 +11,15 @@ am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" = "'"$WRONG_EMAIL"'" ]
 then
-    cn="$CORRECT_NAME"
-    cm="$CORRECT_EMAIL"
+    cn="'"$CORRECT_NAME"'"
+    cm="'"$CORRECT_EMAIL"'"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" = "'"$WRONG_EMAIL"'" ]
 then
-    an="$CORRECT_NAME"
-    am="$CORRECT_EMAIL"
+    an="'"$CORRECT_NAME"'"
+    am="'"$CORRECT_EMAIL"'"
 fi
 
 export GIT_AUTHOR_NAME="$an"
